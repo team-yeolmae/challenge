e6f687c5b7b38218d1bcd04e9b8c1293adc3c122
DESKTOP-GH4LTIO\hi <gps95wwkhhev@gmail.com>
2024-02-26 18:48:34 +0900

    [Feat] 단일 조회 기능 생성 기능설명 : ChallengeService, ReadChallengeResponse, ChallengeRepository 추가

---
+++ src/main/java/org/yeolmae/challenge/domain/dto/ReadChallengeResponse.java
@@ -0,0 +1,28 @@
+package org.yeolmae.challenge.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ReadChallengeResponse {
+
+    private int challenge_id;
+
+    private String title;
+
+    private String writer;
+
+    private String content;
+
+    private LocalDate register_date;
+
+    private LocalDate start_date;
+
+    private LocalDate end_date;
+}
