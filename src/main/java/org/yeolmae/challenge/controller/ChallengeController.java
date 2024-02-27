e34ae0ad6c89dabe5b0e68f0e20b8c69876b5998
DESKTOP-GH4LTIO\hi <gps95wwkhhev@gmail.com>
2024-02-27 15:39:36 +0900

    [Feat] admin 업데이트 기능 수정 수정 내용 : 컨트롤러와 서비스의 메소드명을  UpdateChallnege, ChallnegeUpdate로 수정 수정 이유 : 헷갈리지 않게 하기 위함

--- src/main/java/org/yeolmae/challenge/controller/ChallengeController.java
+++ src/main/java/org/yeolmae/challenge/controller/ChallengeController.java
@@ -9,8 +9,7 @@ import org.yeolmae.challenge.domain.dto.UpdateChallengeRequest;
 import org.yeolmae.challenge.domain.dto.UpdateChallengeResponse;
 import org.yeolmae.challenge.service.ChallengeService;
 
-@Controller
-@ResponseBody
+@RestController
 @RequestMapping("/api/v1/posts")
 @RequiredArgsConstructor
 public class ChallengeController {
@@ -18,10 +17,10 @@ public class ChallengeController {
     private final ChallengeService challengeService;
 
     @PutMapping("/{challenge_id}")
-    public ResponseEntity<UpdateChallengeResponse> postUpdate(@PathVariable Integer challenge_id,
+    public ResponseEntity<UpdateChallengeResponse> UpdateChallenge(@PathVariable Integer challenge_id,
                                                               @RequestBody UpdateChallengeRequest request){
 
-        UpdateChallengeResponse response = challengeService.updatePost(challenge_id, request);
+        UpdateChallengeResponse response = challengeService.ChallengeUpdate(challenge_id, request);
 
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
