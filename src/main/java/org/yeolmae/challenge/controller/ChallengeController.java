d540cb93864870a7fcb2d2b8a8c9708913adb65f
DESKTOP-GH4LTIO\hi <gps95wwkhhev@gmail.com>
2024-02-27 17:04:24 +0900

    [Feat] admin 삭제 기능

--- src/main/java/org/yeolmae/challenge/controller/ChallengeController.java
+++ src/main/java/org/yeolmae/challenge/controller/ChallengeController.java
@@ -1,20 +1,13 @@
 package org.yeolmae.challenge.controller;
-
-import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
-import org.yeolmae.challenge.domain.Challenge;
 import org.yeolmae.challenge.domain.dto.DeleteChallengeResponse;
-import org.yeolmae.challenge.repository.ChallengeRepository;
 import org.yeolmae.challenge.service.ChallengeService;
 
-@Controller
-@ResponseBody
-@RequestMapping("/api/v1/posts")
+@RestController
+@RequestMapping("/challenge")
 @RequiredArgsConstructor
 public class ChallengeController {
 
