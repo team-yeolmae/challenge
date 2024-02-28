84b80e93ce0619293d2c3e4c4938f93abd1f563e
DESKTOP-IFMN175\804-03 <mangoperry2015@gmail.com>
2024-02-28 18:54:02 +0900

    [Fix] domain entity 수정 및 필드 추가
    기능설명
    - 회원데이터 내 badge와 level 개념 다시 적용
    - Member 엔티티 < 기존에 누락된 level 필드 추가
    - History, ChallengeImage 테이블 추가
    - 주요 필드 nullable = false 처리
    - FK, @OneToOne, @ManyToOne 처리

---
+++ src/main/java/org/yeolmae/challenge/domain/ChallengeImage.java
@@ -0,0 +1,23 @@
+package org.yeolmae.challenge.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+public class ChallengeImage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private int id;
+
+    @ManyToOne(cascade = CascadeType.REMOVE)
+    @JoinColumn(name = "challengeId", referencedColumnName = "id")
+    Challenge challenge;
+
+    @Column(name = "image_detail", length = 500)
+    private String image_detail;
+
+    @Column(name = "image_thumb", nullable = false, length = 500)
+    private String image_thumb;
+
+}
