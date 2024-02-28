950f322385f9a8a76c9f12e7afefd14447d57053
DESKTOP-GH4LTIO\hi <gps95wwkhhev@gmail.com>
2024-02-28 18:03:25 +0900

    [Feat] user reply 댓글 등록 기능 기능 요약 : 댓글을 등록할 수 있는 기능

---
+++ src/main/java/org/yeolmae/challenge/controller/ReplyController.java
@@ -0,0 +1,32 @@
+package org.yeolmae.challenge.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.yeolmae.challenge.domain.dto.CreateReplyRequest;
+import org.yeolmae.challenge.domain.dto.CreateReplyResponse;
+import org.yeolmae.challenge.service.ReplyService;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/reply")
+@RequiredArgsConstructor
+public class ReplyController {
+
+    private final ReplyService replyService;
+
+    @PostMapping("/register")
+    public ResponseEntity<CreateReplyResponse> challengeCreate(@RequestBody CreateReplyRequest request) {
+
+        CreateReplyResponse response = replyService.createReply(request);
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+}
---
+++ src/main/java/org/yeolmae/challenge/service/ReplyService.java
@@ -0,0 +1,36 @@
+package org.yeolmae.challenge.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.yeolmae.challenge.domain.Reply;
+import org.yeolmae.challenge.domain.dto.CreateReplyRequest;
+import org.yeolmae.challenge.domain.dto.CreateReplyResponse;
+import org.yeolmae.challenge.repository.ReplyRepository;
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class ReplyService {
+
+    private final ReplyRepository replyRepository;
+
+
+    @Transactional
+    public CreateReplyResponse createReply(CreateReplyRequest request) {
+
+        Reply reply = Reply.builder()
+                .replyer(request.getReplyer())
+                .replyText(request.getReplyText())
+                .build();
+
+        Reply savedReply = replyRepository.save(reply);
+
+        return new CreateReplyResponse(
+                savedReply.getRno(),
+                savedReply.getReplyText(),
+                savedReply.getReplyer()
+        );
+    }
+
+
+}
