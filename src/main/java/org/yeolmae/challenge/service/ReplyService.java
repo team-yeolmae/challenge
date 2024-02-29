175e0b90d4bc0c2692e0b0ee70cc09c50aa937d7
DESKTOP-GH4LTIO\hi <gps95wwkhhev@gmail.com>
2024-02-29 12:05:03 +0900

    [Feat] user reply 댓글 전체 조회 기능 수정 : ReadRepository -> ReplyRepository

--- src/main/java/org/yeolmae/challenge/service/ReplyService.java
+++ src/main/java/org/yeolmae/challenge/service/ReplyService.java
@@ -7,17 +7,17 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.yeolmae.challenge.domain.Reply;
 import org.yeolmae.challenge.domain.dto.ReadReplyResponse;
-import org.yeolmae.challenge.repository.ReadRepository;
+import org.yeolmae.challenge.repository.ReplyRepository;
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class ReplyService {
 
-    private final ReadRepository repository;
+    private final ReplyRepository replyRepository;
 
     public Page<ReadReplyResponse> readAllReply(Pageable pageable) {
 
-        Page<Reply> replyPage = repository.findAll(pageable);
+        Page<Reply> replyPage = replyRepository.findAll(pageable);
 
         return replyPage.map(reply -> new ReadReplyResponse(reply.getRno(), reply.getReplyer(), reply.getReplyText()));
     }
