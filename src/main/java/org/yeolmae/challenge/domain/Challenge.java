f7fca18c1f6b5c24890cc5971538c3e1e9d1befb
thdrkfka <ssongjiaran@gmail.com>
2024-03-01 17:12:04 +0900

    [Refactor] Challenge 파일 추가

---
+++ src/main/java/org/yeolmae/challenge/domain/Challenge.java
@@ -0,0 +1,49 @@
+package org.yeolmae.challenge.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Builder
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Challenge {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "writer", nullable = false)
+    private String writer;
+
+    @Column(name = "content", nullable = false, length = 500)
+    private String content;
+
+    @Column(name = "registerDate", updatable = false, nullable = false)
+    private LocalDate registerDate;
+
+    @Column(name = "startDate", nullable = false)
+    private LocalDate startDate;
+
+    @Column(name = "endDate", nullable = false)
+    private LocalDate endDate;
+
+    public void update(String title, String writer, String content, LocalDate startDate, LocalDate endDate) {
+        this.title = title;
+        this.writer = writer;
+        this.content = content;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    @OneToOne
+    private History history;
+
+}
