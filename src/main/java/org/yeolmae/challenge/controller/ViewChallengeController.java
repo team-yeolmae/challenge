943ee8df1c07f6130da3feb4a6270a999d0a6b2a
DESKTOP-GH4LTIO\hi <gps95wwkhhev@gmail.com>
2024-03-05 19:09:46 +0900

    [Feat] (view) user 홈, 상세페이지 화면 구현

--- src/main/java/org/yeolmae/challenge/controller/ViewChallengeController.java
+++ src/main/java/org/yeolmae/challenge/controller/ViewChallengeController.java
@@ -2,9 +2,11 @@ package org.yeolmae.challenge.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.yeolmae.challenge.domain.dto.PageRequestDTO;
 import org.yeolmae.challenge.domain.dto.PageResponseDTO;
@@ -19,14 +21,27 @@ public class ViewChallengeController {
 
     private final ChallengeService challengeService;
 
-    @GetMapping("/main")
-    public void list(PageRequestDTO pageRequestDTO, Model model){
+        @GetMapping("/main")
+        public void list(PageRequestDTO pageRequestDTO, Model model){
 
-        PageResponseDTO<ReadChallengeResponse> challengeDTO =
-                challengeService.readAllChallenge(pageRequestDTO);
+            PageResponseDTO<ReadChallengeResponse> challengeDTO =
+                    challengeService.readAllChallenge(pageRequestDTO);
 
 
+            model.addAttribute("challengeDTO", challengeDTO);
+
+        }
+
+    @GetMapping("/detail{id}")
+    public String viewChallengeDetail(@PathVariable Integer id, Model model) {
+
+        // id에 해당하는 도전과제 정보를 가져옴
+        ReadChallengeResponse challengeDTO = challengeService.readChallengeById(id);
+
+        // 모델에 도전과제 정보를 추가하여 Thymeleaf에서 사용할 수 있도록 함
         model.addAttribute("challengeDTO", challengeDTO);
 
+        // 상세 페이지 템플릿의 경로를 반환
+        return "challenge/detail"; // 이 경로는 상황에 따라 변경될 수 있습니다.
     }
 }
