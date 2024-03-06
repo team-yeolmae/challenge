17c4012182a774590d7cf5d72a4991758ef108fc
DESKTOP-GH4LTIO\hi <gps95wwkhhev@gmail.com>
2024-03-06 12:45:48 +0900

    [Feat] (view) user 홈, 상세페이지 화면 구현 추가 추가 기능 : 전체목록과 인기목록으로 나누어서 컨트롤러 수정, 로고 추가 PageRequest1DTO(인기목록) 와 PageRequest1DTO(인기목록) 추가

--- src/main/java/org/yeolmae/challenge/controller/ViewChallengeController.java
+++ src/main/java/org/yeolmae/challenge/controller/ViewChallengeController.java
@@ -2,15 +2,12 @@ package org.yeolmae.challenge.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.yeolmae.challenge.domain.dto.PageRequestDTO;
-import org.yeolmae.challenge.domain.dto.PageResponseDTO;
-import org.yeolmae.challenge.domain.dto.ReadChallengeResponse;
+import org.yeolmae.challenge.domain.dto.*;
 import org.yeolmae.challenge.service.ChallengeService;
 
 @Controller
@@ -22,13 +19,17 @@ public class ViewChallengeController {
     private final ChallengeService challengeService;
 
         @GetMapping("/main")
-        public void list(PageRequestDTO pageRequestDTO, Model model){
+        public void list(PageRequest1DTO pageRequest1DTO, Model model){
 
-            PageResponseDTO<ReadChallengeResponse> challengeDTO =
-                    challengeService.readAllChallenge(pageRequestDTO);
-
+            PageResponse1DTO<ReadChallengeResponse> challengeDTO =
+                    challengeService.readAllChallenge1(pageRequest1DTO);
+            // 인기 챌린지 목록 가져오기 (페이지 크기: 4)
+            PageResponse1DTO<ReadChallengeResponse> popularChallengesDTO =
+                    challengeService.readPopularChallenges(PageRequest1DTO.builder().size(4).build());
 
             model.addAttribute("challengeDTO", challengeDTO);
+            model.addAttribute("popularChallengesDTO", popularChallengesDTO);
+
 
         }
 
