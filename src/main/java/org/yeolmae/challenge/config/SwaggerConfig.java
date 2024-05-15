573dc6825591d18199f7c87f25605e759b7dbe5a
PARKCY\Park <mangoperry2015@gmail.com>
2024-05-16 00:34:07 +0900

    [fix] Swagger - Security 설정 추가 - SwaggerConfig 수정

--- src/main/java/org/yeolmae/challenge/config/SwaggerConfig.java
+++ src/main/java/org/yeolmae/challenge/config/SwaggerConfig.java
@@ -2,11 +2,17 @@ package org.yeolmae.challenge.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
-import org.springdoc.core.models.GroupedOpenApi;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.multipart.support.StandardServletMultipartResolver;
+
+import java.util.Arrays;
+import java.util.List;
 
 // @OpenAPIDefinition : swagger에서 제공하는 작성법
 @OpenAPIDefinition(
@@ -15,17 +21,49 @@ import org.springframework.web.multipart.support.StandardServletMultipartResolve
 @Configuration
 public class SwaggerConfig {
 
+    // 토큰이 필요하지 않은 API URL
+    List<String> list = Arrays.asList(
+//            "/api/v1/auth/login",
+//            "/api/v1/members/user",
+//            "/api/v1/members/admin",
+//            "/api/v1/home/postList",
+//            "/api/v1/home/communityList",
+//            "/api/v1/home/popularCommunityList"
+    );
+
     @Bean
-    public GroupedOpenApi firstOpenApi() {
+    public OpenAPI oepnAPI() {
+        SecurityScheme securityScheme = getSecurityScheme();
+        SecurityRequirement securityRequirement = getSecurityRequirement();
 
-        String[] path = {
-                "org.yeolmae.challenge.controller"
-        };
+        return new OpenAPI()
+                .components(new Components().addSecuritySchemes("bearerAuth", securityScheme))
+                .security(List.of(securityRequirement));
+    }
 
-        return GroupedOpenApi.builder()
-                .group("챌린지 어플 API")
-                .packagesToScan(path)
-                .build();
+    // 보안 관련 헤더 추가를 위한 설정
+    private SecurityScheme getSecurityScheme() {
+        return new SecurityScheme()
+                .name("Authorization")
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER);
+    }
+
+    // 각 API에 대한 보안 요구 지정
+    private SecurityRequirement getSecurityRequirement () {
+        SecurityRequirement securityRequirement = new SecurityRequirement();
+
+        // 보안이 필요한 API에 대한 SecurityRequirement 생성
+        securityRequirement.addList("bearerAuth");
+
+        // 보안이 필요하지 않은  API 제외처리
+        for (String endpoint : list) {
+            securityRequirement.remove(endpoint);
+        }
+
+        return securityRequirement;
     }
 
     @Bean
